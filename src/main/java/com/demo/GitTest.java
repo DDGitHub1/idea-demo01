cd8d6cc6d337c2e720ec387d64d97534546f628e
GitHubGits <1210248788@qq.com>
2022-06-26 17:46:32 +0800

    master 2

--- src/main/java/com/demo/GitTest.java
+++ src/main/java/com/demo/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Hello git2");
         System.out.println("Hello git3");
         System.out.println("Hello git new fix");
+        System.out.println("Hello git master test");
     }
 }
